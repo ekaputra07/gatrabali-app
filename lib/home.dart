11022d207842695d4836a23f017f6f944af6cc74
Eka Putra <ekaputra07@gmail.com>
2019-02-22 22:41:30 +1100

    Initial Firestore implementation.

--- lib/home.dart
+++ lib/home.dart
@@ -19,30 +19,38 @@ class _HomeState extends State<Home> {
 }
 
 Widget _buildBody(BuildContext ctx) {
-  // TODO: Pull news from firestore
-  const list = [
-    {"title": "Apa kabar dunia"},
-    {"title": "Hello Hello"},
-    {"title": "Bagus sekali"},
-  ];
-  return _buildList(ctx, list);
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance
+        .collection("entries")
+        .orderBy("id", descending: true)
+        .limit(20)
+        .snapshots(),
+    builder: (ctx, snapshots) {
+      if (!snapshots.hasData) return LinearProgressIndicator();
+
+      return _buildList(ctx, snapshots.data.documents);
+    },
+  );
 }
 
-Widget _buildList(BuildContext ctx, List<Map> news) {
+Widget _buildList(BuildContext ctx, List<DocumentSnapshot> docs) {
   return ListView(
       padding: new EdgeInsets.only(top: 10.0),
-      children: news.map((n) => _listItem(ctx, n)).toList());
+      children: docs.map((doc) => _listItem(ctx, doc)).toList());
 }
 
-Widget _listItem(BuildContext ctx, Map item) {
+Widget _listItem(BuildContext ctx, DocumentSnapshot item) {
+  Map<String, dynamic> data = item.data;
+
   return Padding(
-      key: ValueKey(item["title"]),
+      key: ValueKey(item.documentID),
       padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
       child: Container(
         decoration: BoxDecoration(
             border: Border.all(color: Colors.grey),
             borderRadius: BorderRadius.circular(5.0)),
         child: ListTile(
-            title: Text(item["title"]), onLongPress: () => print(item["name"])),
+            title: Text(data["title"]),
+            onLongPress: () => print(data["title"])),
       ));
 }
