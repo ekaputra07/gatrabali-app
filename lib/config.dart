fbbde3fc47fa6261ec79ff332a1b868410e85eda
Eka Putra <ekaputra07@gmail.com>
2019-06-30 18:25:06 +1000

    Bumped version to 1.0.5.

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 const API_HOST = "https://gatrabali.appspot.com";
-const APP_VERSION = "1.0.4";
+const APP_VERSION = "1.0.5";
 const FEED_SOURCES = "balipost.com, metrobali.com, balipuspanews.com";
 
 const ANDROID_APP_ID = "com.gatrabali.android";
