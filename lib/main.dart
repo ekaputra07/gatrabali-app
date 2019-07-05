5e0b44a9c21ba94d3f883498ace42a19371af8d7
Eka Putra <ekaputra07@gmail.com>
2019-07-05 21:57:49 +1000

    Hide debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Gatra Bali',
         theme: ThemeData(primarySwatch: Colors.green),
         onGenerateRoute: _generateRoute,
