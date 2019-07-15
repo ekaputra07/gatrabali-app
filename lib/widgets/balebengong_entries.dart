0f26910f5e40c198b9a39b0818f0458598d4491c
Eka Putra <ekaputra07@gmail.com>
2019-07-15 23:51:33 +1000

    Implemented BaleBengong category entries.

---
+++ lib/widgets/balebengong_entries.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:gatrabali/models/entry.dart';
+import 'package:gatrabali/repository/entries.dart';
+import 'package:gatrabali/widgets/single_news_nocard.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+class BalebengongEntries extends StatefulWidget {
+  final int categoryId;
+  final String name;
+
+  BalebengongEntries(this.categoryId, this.name);
+
+  @override
+  _BalebengongEntriesState createState() => _BalebengongEntriesState();
+}
+
+class _BalebengongEntriesState extends State<BalebengongEntries>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  RefreshController _refreshController;
+  StreamSubscription _sub;
+  List<Entry> _entries = <Entry>[];
+  int _cursor = 0;
+
+  @override
+  void initState() {
+    _refreshController = RefreshController(initialRefresh: false);
+    _refreshEntries();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_sub != null) {
+      _sub.cancel();
+    }
+    _refreshController.dispose();
+    super.dispose();
+  }
+
+  void _refreshEntries() {
+    _sub = EntryService.fetchBalebengongEntries(categoryId: widget.categoryId)
+        .asStream()
+        .listen((entries) {
+      _refreshController.refreshCompleted();
+      setState(() {
+        _cursor = entries.last.publishedAt;
+        _entries = entries;
+      });
+    });
+    _sub.onError((err) {
+      _refreshController.refreshFailed();
+      print(err);
+    });
+  }
+
+  void _loadEntries() {
+    _sub = EntryService.fetchBalebengongEntries(
+            categoryId: widget.categoryId, cursor: _cursor)
+        .asStream()
+        .listen((entries) {
+      _refreshController.loadComplete();
+      setState(() {
+        _cursor = entries.last.publishedAt;
+        _entries.addAll(entries);
+      });
+    });
+    _sub.onError((err) {
+      _refreshController.loadNoData();
+      print(err);
+    });
+  }
+
+  Widget _listItem(BuildContext ctx, Entry entry) {
+    return Padding(
+        padding: new EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        child: SingleNewsNoCard(
+            key: ValueKey(entry.id),
+            entry: entry,
+            showCategoryName: widget.categoryId == 0));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    return SmartRefresher(
+      controller: _refreshController,
+      enablePullDown: true,
+      enablePullUp: true,
+      onRefresh: () {
+        _refreshEntries();
+      },
+      onLoading: () {
+        _loadEntries();
+      },
+      child: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        addAutomaticKeepAlives: true,
+        children: _entries.map((entry) => _listItem(context, entry)).toList());
+  }
+}
