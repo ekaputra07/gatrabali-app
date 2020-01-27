d7aa582418b5da07b43b94b9bb03ab6aca65eeae
Eka Putra <ekaputra07@gmail.com>
2020-01-27 21:07:50 +1100

    Bumped app version to 1.0.10

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 const API_HOST = "https://gatrabali.appspot.com/api/v2";
-const APP_VERSION = "1.0.9";
+const APP_VERSION = "1.0.10";
 const FEED_SOURCES = "balebengong.id, balipost.com, balipuspanews.com, jawapos.com, metrobali.com";
 const DEFAULT_AVATAR_IMAGE = "https://firebasestorage.googleapis.com/v0/b/gatrabali.appspot.com/o/app%2Favatar.png?alt=media";
 const KRIMINAL_CATEGORY_ID = 11;
